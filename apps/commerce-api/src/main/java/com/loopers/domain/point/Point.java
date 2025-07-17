efeb26a50e9813630f0c164401bc28b760fb14a1
SoohoPark <clapp301@gmail.com>
2025-07-17 21:44:09 +0900

    feat: Point 엔티티에 charge 메서드 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/point/Point.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/Point.java
@@ -1,5 +1,7 @@
 package com.loopers.domain.point;
 
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -18,5 +20,15 @@ public class Point {
     public Point(String userId) {
         this.userId = userId;
         this.amount = 0L;
+    }
+
+    public void charge(Long chargeAmount) {
+        if (chargeAmount <= 0) {
+            throw new CoreException(ErrorType.BAD_REQUEST,
+                "0 이하의 값은 충전이 불가합니다."
+            );
+        }
+
+        this.amount += chargeAmount;
     }
 }
