223775f38660fba40469cdc229ef17a2b5b2ca68
SoohoPark <clapp301@gmail.com>
2025-07-31 17:04:20 +0900

    feat: Order 서비스 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/order/OrderService.java
@@ -0,0 +1,49 @@
+package com.loopers.domain.order;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public Order findById(Long id) {
+        Order order = orderRepository.findById(id);
+
+        if (order == null) {
+            throw new CoreException(
+                ErrorType.NOT_FOUND,
+                "주문이 존재하지 않습니다."
+            );
+        }
+
+        return order;
+    }
+
+    public Order findByIdAndUserId(Long orderId, String userId) {
+        Order order = orderRepository.findByIdAndUserId(orderId, userId);
+
+        if (order == null) {
+            throw new CoreException(
+                ErrorType.NOT_FOUND,
+                "주문이 존재하지 않습니다."
+            );
+        }
+
+        return order;
+    }
+
+    public List<Order> findByUserId(String userId) {
+        return orderRepository.findByUserIdOrderByOrderedAtDesc(userId);
+    }
+}
